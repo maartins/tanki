22c44dcb94b4fd7a1306f01817475ea5e2ef9c98
maartins <maartins@5343cc99-dadb-414b-924b-1031fa5fff23>
2015-08-11 16:33:18 +0000

    comments

--- TANK/src/Bullet.java
+++ TANK/src/Bullet.java
@@ -1,117 +1,117 @@
 
 public class Bullet extends GameObject{
 	
-	private int disx;
-	private int disy;
-	private int curDirection;
-	private int damage;
+	private int x;					  // Bullets current x position
+	private int y;				  	  // Bullets current y position
+	private int direction;		  	  // Bullets moving direction
+	private int damage;               // Bullets damage  
 	
-	private final int SPEED = 5;
-	private final int MAXDIST = 500;
+	private final int SPEED = 5;	  // Bullets speed
+	private final int MAXDIST = 500;  // Bullets maximum travel distance
 	
-	private boolean maxDistReached;
-	private boolean collide;
+	private boolean isMaxDistReached; // Check if the maximum distance is reached
+	private boolean isCollision;	  // Check if bullet collides
 	
-	public Bullet(int posX, int posY, int curDir, int damage, String name, String imagePath){
-		super(posX, posY, name, imagePath);
-		disx = posX;
-		disy = posY;
+	public Bullet(int posX, int posY, int curDir){
+		super(posX, posY, "Bullet", "Images//Bullet01.png");
+		x = posX;
+		y = posY;
 		
-		curDirection = curDir;
+		direction = curDir;
 		
-		this.damage = damage;
+		damage = 1;
 		
-		maxDistReached = false;
+		isMaxDistReached = false;
 		
-		collide = false;
+		isCollision = false;
 	}
 	
 	public Bullet(int posX, int posY, int curDir, String name){
 		super(posX, posY, name, "Images//Bullet01.png");
-		disx = posX;
-		disy = posY;
+		x = posX;
+		y = posY;
 		
-		curDirection = curDir;
+		direction = curDir;
 		
 		damage = 1;
 		
-		maxDistReached = false;
+		isMaxDistReached = false;
 		
-		collide = false;
+		isCollision = false;
 	}
 	
-	public Bullet(int posX, int posY, int curDir){
-		super(posX, posY, "Bullet", "Images//Bullet01.png");
-		disx = posX;
-		disy = posY;
+	public Bullet(int posX, int posY, int curDir, int damage, String name, String imagePath){
+		super(posX, posY, name, imagePath);
+		x = posX;
+		y = posY;
 		
-		curDirection = curDir;
+		direction = curDir;
 		
-		damage = 1;
+		this.damage = damage;
 		
-		maxDistReached = false;
+		isMaxDistReached = false;
 		
-		collide = false;
+		isCollision = false;
 	}
 	
 	public void move(){
-		switch(curDirection){
-		case 0:
-			this.setX(this.getX() + SPEED);
-			if(this.getX() - disx >= MAXDIST){
-				maxDistReached = true;
-			}
-			break;
-		case 1:
-			this.setY(this.getY() - SPEED);
-			if(this.getY() - disy <= -MAXDIST){
-				maxDistReached = true;
-			}
-			break;
-		case 2:
-			this.setX(this.getX() - SPEED);
-			if(this.getX() - disx <= -MAXDIST){
-				maxDistReached = true;
-			}
-			break;
-		case 3:
-			this.setY(this.getY() + SPEED);
-			if(this.getY() - disy >= MAXDIST){
-				maxDistReached = true;
-			}
-			break;
+		switch(direction){
+			case 0:
+				this.setX(this.getX() + SPEED);
+				if(this.getX() - x >= MAXDIST){
+					isMaxDistReached = true;
+				}
+				break;
+			case 1:
+				this.setY(this.getY() - SPEED);
+				if(this.getY() - y <= -MAXDIST){
+					isMaxDistReached = true;
+				}
+				break;
+			case 2:
+				this.setX(this.getX() - SPEED);
+				if(this.getX() - x <= -MAXDIST){
+					isMaxDistReached = true;
+				}
+				break;
+			case 3:
+				this.setY(this.getY() + SPEED);
+				if(this.getY() - y >= MAXDIST){
+					isMaxDistReached = true;
+				}
+				break;
 		}
 		
 		for(Block b : MainPanel.map.getBlockList()){
 			if(this.getBounds().intersects(b.getBounds()) && b.isShootable()){
-		    	collide = true;
-		    	b.recieveDamage(8, curDirection);
+		    	isCollision = true;
+		    	b.recieveDamage(8, direction);
 		    }
 		}
 		
 		for(Enemy e : MainPanel.enemies){
 			if(this.getBounds().intersects(e.getBounds())){
 				e.recieveDamage(damage);
-		    	collide = true;
+		    	isCollision = true;
 		    }
 		}
 		
 		if(this.getBounds().intersects(MainPanel.tank.getBounds())){
 			MainPanel.tank.recieveDamage(damage);
-	    	collide = true;
+	    	isCollision = true;
 	    }
 		
 		if(this.getBounds().intersects(MainPanel.bird.getBounds())){
 			MainPanel.bird.recieveDamage(damage);
-			collide = true;
+			isCollision = true;
 		}
 	}
 	
 	public boolean isMaxDistReached(){
-		return maxDistReached;
+		return isMaxDistReached;
 	}
 	
 	public boolean isCollision(){
-		return collide;
+		return isCollision;
 	}
 }
